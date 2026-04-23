660ccceef6d1fd7c6add466f2c3b1f8950426dca
arise9643-star <arise9643@gmail.com>
2026-04-23 11:15:00 +0300

    rename file

--- roast_bot.py
+++ roast_bot.py
@@ -1,19 +1,28 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
 import groq
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
+
+app = FastAPI()
 client = groq.Groq(api_key=os.getenv("GROQ_API_KEY"))
-conversation_history = [{"role":"assistant","content":"you are a ruthless roast bot"}]
-while True:
-    user_input = input("Enter message to get roasted or type 'quit' to exit: ")
-    if user_input.lower() == "quit":
-        print("GoodBye!!")
-        break
-    else:
-        conversation_history.append({"role":"user","content":user_input})
-        response = client.chat.completions.create(
-            model="llama-3.3-70b-versatile",
-            messages= conversation_history
-        )
-        reply = response.choices[0].message.content
-        print(f"AI ROAST_BOT: {reply}")
+
+conversation_history = [
+    {"role": "system", "content": "You are RoastBot. Roast the user savagely but keep it funny."}
+]
+
+class UserMessage(BaseModel):
+    message: str
+
+@app.post("/chat")
+def chat(user_message: UserMessage):
+    conversation_history.append({"role": "user", "content": user_message.message})
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=conversation_history
+    )
+    reply = response.choices[0].message.content
+    conversation_history.append({"role": "assistant", "content": reply})
+    return {"reply": reply}
